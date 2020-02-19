b8e8f833f2afb5283ce2c2200bcf011eac514465
jwham <jwham@popcorn5.com>
2020-02-19 11:53:25 +0900

    타입스크립트 강좌

---
+++ typescript-basic/test.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Test = /** @class */ (function () {
+    function Test() {
+        console.log('test');
+        console.log('test');
+        console.log('test');
+    }
+    return Test;
+}());
+new Test();
---
+++ typescript-basic/test.ts
@@ -0,0 +1,15 @@
+
+
+class Test {
+    constructor (){
+
+        console.log('test');
+        console.log('test');
+        console.log('test');
+    }
+}
+
+new Test();
+
+
+export{}
